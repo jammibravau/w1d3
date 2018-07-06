ca8351633f57cca94f8cab1d26ea67f01e1b5543
Michael J. Martins-Baptista <mjmartinsbaptista@gmail.com>
2018-07-06 13:34:49 -0400

    Create display method for students hash

---
+++ exercise7.rb
@@ -0,0 +1,13 @@
+students = {
+  cohort1: 34,
+  cohort2: 42,
+  cohort3: 22,
+}
+
+def display(list)
+  list.each do |key, value|
+    puts "#{key}: #{value} students"
+  end
+end
+
+display(students)
