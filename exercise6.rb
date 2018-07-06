5e1cb26c3cce91cf3653b24a1d8248d75d8124bf
Michael J. Martins-Baptista <mjmartinsbaptista@gmail.com>
2018-07-06 11:33:49 -0400

    create final methods for exercise

--- exercise6.rb
+++ exercise6.rb
@@ -1,11 +1,39 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-
-
 def display_list(list)
   list.each do |item|
     puts "* #{item}"
   end
 end
 
-display_list(grocery_list)
+def add_item(list, item)
+  list << item
+  return list
+end
+
+def count(list)
+  puts "#{list.count}"
+  return list.count
+end
+
+def search(list, word)
+  if list.include?(word)
+    puts "You need to pick up #{word}!"
+  else
+    puts "You don't need #{word}."
+  end
+  return word
+end
+
+def display_item(list, item_number)
+  puts "* #{list[item_number]}"
+  return list[item_number]
+end
+
+def sort(list)
+ list.sort!
+end
+
+def delete(list, item)
+  list.delete(item)
+end
